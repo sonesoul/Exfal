2f918a92a53201f1ea98b08909c5d01758bdef15
sonesoul <smippy098@gmail.com>
2026-07-01 08:01:43 -0600

    add conversion to RectangleF extension

---
+++ Extensions/RectangleExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Exfal.Drawing;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exfal.Extensions
+{
+    public static class RectangleExtensions
+    {
+        public static RectangleF ToRectangleF(in this Rectangle r)
+        {
+            return new(r.X, r.Y, r.Width, r.Height);
+        } 
+
+    }
+}
